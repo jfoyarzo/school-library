487cc2fed80e8dbdd996e6e52424b493328d476a
jfoyarzo <foyarzo621@gmail.com>
2023-01-02 18:34:54 -0300

    Add instance variable for parent_permission to the Person class

--- person.rb
+++ person.rb
@@ -1,20 +1,21 @@
 class Person
-  def initialize(age, name = 'Unknown', _parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
+    @parent_permission = parent_permission
   end
 
   attr_reader :id
   attr_accessor :name, :age
 
   def can_use_services?
-    is_of_age? || _parent_permission
+    is_of_age? || @parent_permission
   end
 
   private
 
   def is_of_age?
-    @age > 18
+    @age >= 18
   end
 end

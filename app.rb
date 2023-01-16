9115aaf32995cace00e5204eab1f6098cd0cb6f4
Matyas Gombos <gombosmm@gmail.com>
2023-01-16 12:54:05 -0500

    Remove all modules from app

--- app.rb
+++ app.rb
@@ -1,7 +1,17 @@
-require_relative 'modules/people_creation'
+require_relative 'modules/create_people'
+require_relative 'modules/create_book'
+require_relative 'modules/create_rental'
+require_relative 'modules/list_books'
+require_relative 'modules/list_people'
+require_relative 'modules/list_rentals'
 
 class App
   include CreatePeople
+  include CreateBook
+  include CreateRental
+  include ListBooks
+  include ListPeople
+  include ListRentals
 
   def initialize()
     @people = []
@@ -9,52 +19,25 @@ class App
     @classrooms = []
   end
 
-  def list_books
-    puts
-    @books.each { |book| puts "Author: #{book.author}, Title: #{book.title}" }
-  end
-
-  def list_people
-    puts
-    @people.each { |person| puts "[#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
-  end
-
-  def create_book
-    print 'Title: '
-    title = gets.chomp
-    print 'Author: '
-    author = gets.chomp
-    book = Book.new(title, author)
-    @books.push(book)
-    puts 'Book added successfully'
-  end
-
-  def create_rental
-    puts 'Select person by option number:'
-    @people.each_with_index do |person, index|
-      puts "#{index + 1} - Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
-    end
-    opt_person = gets.chomp.to_i - 1
-    person = @people.at(opt_person)
-    puts 'Select book by option number:'
-    @books.each_with_index { |book, index| puts "#{index + 1} - Author: #{book.author}, Title: #{book.title}" }
-    opt_book = gets.chomp.to_i - 1
-    book = @books.at(opt_book)
-    puts 'Enter date of rental: '
-    date = gets.chomp
-    person.add_rental(date, book)
-    puts 'Rental created successfully'
-  end
-
-  def list_rentals_for_id(id)
-    person = @people.find { |p| p.id == id }
-    if person.rentals
-      puts "-- Rentals for user #{person.name} (ID: #{person.id}) --"
-      person.rentals.each_with_index do |element, index|
-        puts "#{index + 1} - Date: #{element.date}, Book: #{element.book.title}"
-      end
+  def select_option(option)
+    case option
+    when '1'
+      list_books
+    when '2'
+      list_people
+    when '3'
+      select_role
+    when '4'
+      create_book
+    when '5'
+      create_rental
+    when '6'
+      puts 'Enter the ID of the person:'
+      id = gets.chomp.to_i
+      list_rentals_for_id(id)
     else
-      puts 'This user has no rentals'
+      puts 'Thank you for using this app, see you soon!'
+      exit
     end
   end
 end

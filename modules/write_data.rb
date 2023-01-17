3a5da75eddad609f1da2b5d0db402f4a0cace9f0
Matyas Gombos <gombosmm@gmail.com>
2023-01-17 16:34:48 -0500

    Add generate json methods to write_data

--- modules/write_data.rb
+++ modules/write_data.rb
@@ -2,10 +2,27 @@ require 'json'
 
 module WriteData
   def write_data(books, people)
-    json = JSON.generate(books)
-    json2 = JSON.generate(people)
-    File.write('./json_files/books.json', json)
+    json1 = JSON.pretty_generate(books.map do |book|
+      {
+        title: book.title,
+        author: book.author
+      }
+    end)
+    File.write('./json_files/books.json', json1)
+
+    json2 = JSON.pretty_generate(people.map do |person|
+      hash = {
+        id: person.id,
+        name: person.name,
+        age: person.age,
+        parent_permission: person.parent_permission,
+        classroom: (person.classroom || nil),
+        specialization: (person.specialization || nil)
+      }
+      hash.compact!
+    end)
     File.write('./json_files/people.json', json2)
+
     puts 'Files written successfully'
   end
 end

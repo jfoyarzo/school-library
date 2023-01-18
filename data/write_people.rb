ac23f95d281e783bbca4f65ecc1ea179a14ed016
jfoyarzo <foyarzo621@gmail.com>
2023-01-18 18:26:00 -0300

    Update write_people method with conditional for instance type

--- data/write_people.rb
+++ data/write_people.rb
@@ -1,15 +1,23 @@
+require_relative '../student'
+require_relative '../teacher'
+
 module WritePeople
   def write_people
     return if @people.empty?
 
     json = JSON.pretty_generate(@people.map do |person|
-      {
+      hash = {
         class: person.class.name,
         id: person.id,
         name: person.name,
         age: person.age,
-        parent_permission: person.parent_permission,
+        parent_permission: person.parent_permission
       }
+
+      hash['classroom'] = person.classroom if person.instance_of? Student
+
+      hash['specialization'] = person.specialization if person.instance_of? Teacher
+      hash
     end)
     File.write('./json_files/people.json', json)
 

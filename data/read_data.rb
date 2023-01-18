209490cfde713be40bf6519f3a1864b6fe423ae0
jfoyarzo <foyarzo621@gmail.com>
2023-01-18 18:26:36 -0300

    Update instantiation of classes on read_people method

--- data/read_data.rb
+++ data/read_data.rb
@@ -4,15 +4,23 @@ require './student'
 module ReadData
   def read_people
     return [] unless File.exist?('./json_files/people.json') && !File.empty?('./json_files/people.json')
+
     json = File.read('./json_files/people.json')
     parsed = JSON.parse(json)
     array = []
     parsed.each do |person|
       if person['class'] == 'Student'
-        person_new = Student.new(person['id'], person['name'], person['age'], parent_permission: person['parent_permission'])
+        person_new = Student.new(
+          person['id'],
+          person['classroom'],
+          person['age'],
+          person['name'],
+          parent_permission: person['parent_permission']
+        )
       end
       if person['class'] == 'Teacher'
-        person_new = Teacher.new(person['id'], person['name'], person['age'], parent_permission: person['parent_permission'])
+        person_new = Teacher.new(person['id'], person['specialization'], person['age'], person['name'],
+                                 parent_permission: person['parent_permission'])
       end
       array << person_new
     end
@@ -21,6 +29,7 @@ module ReadData
 
   def read_books
     return [] unless File.exist?('./json_files/books.json') && !File.empty?('./json_files/books.json')
+
     json = File.read('./json_files/books.json')
     parsed = JSON.parse(json)
     array = []
@@ -35,6 +44,14 @@ module ReadData
     return [] unless File.exist?('./json_files/rentals.json') && !File.empty?('./json_files/rentals.json')
 
     json = File.read('./json_files/rentals.json')
-    JSON.parse(json)
+    rental_list = JSON.parse(json)
+    rental_list.each do |rental|
+      person = @people.find { |per| per.id == rental['id'] }
+      rental['rentals'].each do |rent|
+        book = @books.find { |element| element.title == rent['title'] }
+        Rental.new(rent['date'], book, person)
+      end
+    end
+    rental_list
   end
 end

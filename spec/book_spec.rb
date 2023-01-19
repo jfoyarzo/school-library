a342e897da74a733b7001ca84219c23fdc881497
Matyas Gombos <gombosmm@gmail.com>
2023-01-19 11:04:36 -0500

    Add test to book_spec

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,1 +1,8 @@
-require_relative 'spec_helper'
+require 'spec_helper'
+
+describe '#new' do
+  book = Book.new('Title', 'Author')
+  it 'returns a book object with two parameters' do
+    expect(book).to be_an_instance_of(Book)
+  end
+end

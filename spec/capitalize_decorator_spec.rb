e6459f80b1fec4c090333da1f6aa7d3a672c69d9
Matyas Gombos <gombosmm@gmail.com>
2023-01-19 14:33:51 -0500

    Create decorator tests

--- spec/capitalize_decorator_spec.rb
+++ spec/capitalize_decorator_spec.rb
@@ -1,1 +1,16 @@
 require_relative 'spec_helper'
+
+describe 'test capitalize decorator class' do
+  before :each do
+    @person = Person.new(666, 18,'john')
+    @capitalize_decorator = CapitalizeDecorator.new(@person)
+  end
+
+  it 'should be a kind of BaseDecorator' do
+    expect(@capitalize_decorator).to be_kind_of(BaseDecorator)
+  end
+
+  it 'should capitalize the correct name' do
+    expect(@capitalize_decorator.correct_name).to eql('John')
+  end
+end
--- spec/trimmer_decorator_spec.rb
+++ spec/trimmer_decorator_spec.rb
@@ -1,1 +1,16 @@
 require_relative 'spec_helper'
+
+describe 'test trimmer decorator class' do
+  before :each do
+    @person = Person.new(666, 18, 'johnnyvargas')
+    @trimmer_decorator = TrimmerDecorator.new(@person)
+  end
+
+  it 'should be a kind of BaseDecorator' do
+    expect(@trimmer_decorator).to be_kind_of(BaseDecorator)
+  end
+
+  it 'should trim the correct name' do
+    expect(@trimmer_decorator.correct_name).to eql('johnnyvarg')
+  end
+end

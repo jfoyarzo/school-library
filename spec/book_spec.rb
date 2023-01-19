a15448f52f7aa5b3608b7a73a4c97ba64c59d55e
jfoyarzo <foyarzo621@gmail.com>
2023-01-19 14:32:48 -0300

    Add unit tests for Book class

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,8 +1,22 @@
 require 'spec_helper'
 
-describe '#new' do
-  book = Book.new('Title', 'Author')
-  it 'returns a book object with two parameters' do
-    expect(book).to be_an_instance_of(Book)
+describe Book do
+  before(:each) do
+    @book = Book.new('Test title', 'Test author')
   end
+  describe '#new' do
+    it 'returns a book instance with two parameters' do
+      expect(@book).to be_an_instance_of(Book)
+    end
+  end
+  describe '#title' do
+    it 'returns the correct title' do
+      expect(@book.title).to eql 'Test title'
+    end
+  end
+  describe '#author' do
+    it 'returns the correct author' do
+      expect(@book.author).to eql 'Test author'
+    end
+  end  
 end

0f052e6ab0d4192f2cddc81f172418b14fcce56c
Matyas Gombos <gombosmm@gmail.com>
2023-01-19 13:26:18 -0500

    Create tests forteacher class

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,1 +1,15 @@
 require_relative 'spec_helper'
+
+describe 'test teacher class' do
+  before :each do
+    @teacher = Teacher.new(666, 'Time-Space', 981, 'The Doctor', parent_permission: true)
+  end
+
+  it 'should return a teacher instance with four parameters' do
+    expect(@teacher).to be_an_instance_of(Teacher)
+  end
+
+  it 'should return the correct result of can_use_services?' do
+    expect(@teacher.can_use_services?).to eql(true)
+  end
+end
